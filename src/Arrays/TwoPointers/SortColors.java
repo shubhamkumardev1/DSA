0b992f9104f70003f3838bf1ba05f987bcbbef2a
Shubham Kumar <shubhamcodes.2003@gmail.com>
2026-06-05 08:15:15 +0530

    Dutch National Flag Problem, Sort Colors!

---
+++ src/Arrays/TwoPointers/SortColors.java
@@ -0,0 +1,33 @@
+package Arrays.TwoPointers;
+
+import java.util.Arrays;
+
+public class SortColors {
+    static void main(String[] args) {
+        int[] arr = {2,0,2,1,1,0};
+        int low= 0;
+        int mid= 0;
+        int high= arr.length-1;
+
+        while(mid <= high){
+            if (arr[mid] == 0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+
+                low++;
+                mid++;
+            }
+             else if (arr[mid] == 1){
+                 mid++;
+            } else {
+                 int temp = arr[mid];
+                 arr[mid] = arr[high];
+                 arr[high] = temp;
+
+                 high--;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
