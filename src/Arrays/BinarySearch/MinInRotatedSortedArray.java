3849a189b9378803a11c81656aac1c3d95897287
Shubham Kumar <shubhamcodes.2003@gmail.com>
2026-06-28 13:03:38 +0530

    Binary Search: Minimum in Rotated Sorted Array!

---
+++ src/Arrays/BinarySearch/MinInRotatedSortedArray.java
@@ -0,0 +1,19 @@
+package Arrays.BinarySearch;
+
+public class MinInRotatedSortedArray {
+    static void main() {
+        int[] arr = {4,5,6,7,8,9,0,1,2};
+        int left = 0;
+        int right = arr.length-1;
+
+        while (left<right){
+            int mid = left + (right-left)/2;
+            if (arr[mid] > arr[right]){
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        System.out.println(left);
+    }
+}
