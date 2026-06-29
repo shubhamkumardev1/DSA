19a267a285543dca8400672e9ebc7f1622c8c023
Shubham Kumar <shubhamcodes.2003@gmail.com>
2026-06-29 20:05:33 +0530

    Reinforcement Problem: Find Max'm Element!

---
+++ src/Arrays/Reinforcement/FindMaximumElement.java
@@ -0,0 +1,31 @@
+package Arrays.Reinforcement;
+
+public class FindMaximumElement {
+
+    public static int findMax(int[] arr) {
+
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Array cannot be null or empty");
+        }
+
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 9, 4, 15, 7, 10};
+
+        int maximum = findMax(arr);
+
+        System.out.println("Maximum Element: " + maximum);
+    }
+}
