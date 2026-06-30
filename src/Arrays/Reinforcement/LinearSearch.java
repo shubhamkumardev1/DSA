7a80b3424928e7f5b85f55b165408246f8818c90
Shubham Kumar <shubhamcodes.2003@gmail.com>
2026-06-30 21:32:21 +0530

    Reinforcement Problem: Linear Search!

---
+++ src/Arrays/Reinforcement/LinearSearch.java
@@ -0,0 +1,33 @@
+package Arrays.Reinforcement;
+
+public class LinearSearch {
+
+    public static int linearSearch(int[] arr, int target) {
+
+        // Traverse the entire array
+        for (int i = 0; i < arr.length; i++) {
+
+            // Check if current element matches target
+            if (arr[i] == target) {
+                return i; // Return index if found
+            }
+        }
+
+        // Target not found
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {12, 45, 7, 23, 89, 34};
+        int target = 23;
+
+        int index = linearSearch(arr, target);
+
+        if (index != -1) {
+            System.out.println("Element found at index: " + index);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
