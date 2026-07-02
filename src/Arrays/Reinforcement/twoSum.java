3d1f3d4f670ce9f90276fe2afac4628f731beb2e
Shubham Kumar <shubhamcodes.2003@gmail.com>
2026-07-02 20:45:12 +0530

    Reinforcement Problem: Two Sum!

---
+++ src/Arrays/Reinforcement/twoSum.java
@@ -0,0 +1,23 @@
+package Arrays.Reinforcement;
+
+public class twoSum {
+    static void main() {
+        int[] arr = {1,2,3,5,5,6,7};
+        int target = 11;
+
+        int left = 0;
+        int right = arr.length-1;
+        while (left < right){
+            int sum = arr[left] + arr[right];
+            if (sum == target){
+                System.out.println("Found at index: " + left+" and " +right);
+                break;
+            } else if (sum < target){
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+    }
+}
