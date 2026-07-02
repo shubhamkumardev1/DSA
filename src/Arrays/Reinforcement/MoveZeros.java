3522c95541918dd6db378abacaccb182be69ad0d
Shubham Kumar <shubhamcodes.2003@gmail.com>
2026-07-02 20:38:35 +0530

    Reinforcement Problem: Move Zeros!

---
+++ src/Arrays/Reinforcement/MoveZeros.java
@@ -0,0 +1,20 @@
+package Arrays.Reinforcement;
+
+import java.util.Arrays;
+
+public class MoveZeros {
+    static void main() {
+        int[] arr = {0,1,0,3,12};
+
+        int slow = 0;
+        for (int fast = 0; fast < arr.length; fast++) {
+            if (arr[fast] != 0){
+                int temp = arr[slow];
+                arr[slow] = arr[fast];
+                arr[fast] = temp;
+                 slow++;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
