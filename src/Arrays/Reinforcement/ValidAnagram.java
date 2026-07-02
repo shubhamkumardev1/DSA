4a377fa3f10cc71fc9cf0eadac34a73e05baef8d
Shubham Kumar <shubhamcodes.2003@gmail.com>
2026-07-02 20:54:01 +0530

    Reinforcement Problem: Valid Anagram!

---
+++ src/Arrays/Reinforcement/ValidAnagram.java
@@ -0,0 +1,29 @@
+package Arrays.Reinforcement;
+
+import java.util.HashMap;
+
+public class ValidAnagram {
+    static void main() {
+        String s = "anagram";
+        String t = "nagaram";
+
+        if (s.length() != t.length()){
+            System.out.println("Not Valid!");
+            return;
+        }
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        for (char ch : t.toCharArray()){
+            if (!map.containsKey(ch) || map.get(ch) == 0){
+                System.out.println(false);
+                return;
+            }
+            map.put(ch,map.get(ch)-1);
+        }
+        System.out.println(true);
+    }
+}
